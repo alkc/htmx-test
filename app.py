5db982849e95cd0c5d8d48272214f13de93ec40d
Alexander Koc <alexander.koc@skane.se>
2023-08-31 11:47:30 +0200

    Even more stuff from the tutorial

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # app.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
+
+from todo import todos
 
 app = Flask(__name__)
 
@@ -18,6 +20,21 @@ js.build()  # new
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+
+@app.route("/search", methods=["POST"])
+def search_todo():
+    search_term = request.form.get("search")
+
+    if not len(search_term):
+        return render_template("todo.html", todos=[])
+
+    res_todos = []
+    for todo in todos:
+        if search_term in todo["title"]:
+            res_todos.append(todo)
+
+    return render_template("todo.html", todos=res_todos)
 
 
 if __name__ == "__main__":
