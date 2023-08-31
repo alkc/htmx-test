20e19f6b497b2ce4328d60ce6c971b44002cc83b
Alexander Koc <alexander.koc@skane.se>
2023-08-31 14:35:32 +0200

    Add indicator test

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 # app.py
 
+import time
+
 from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
 
 from todo import todos
 
-app = Flask(__name__)
+# import config
 
-assets = Environment(app)
-css = Bundle("src/main.css", output="dist/main.css")
-js = Bundle("src/*.js", output="dist/main.js")  # new
 
-assets.register("css", css)
-assets.register("js", js)  # new
-css.build()
-js.build()  # new
+def get_app():
+    app = Flask(__name__)
+
+    # app.config.from_object(config.DefaultConfig())  # Note initialization of Config
+
+    assets = Environment(app)
+    css = Bundle("src/main.css", output="dist/main.css")
+    js = Bundle("src/*.js", output="dist/main.js")
+
+    assets.register("css", css)
+    assets.register("js", js)
+    css.build()
+    js.build()
+
+    return app
+
+
+app = get_app()
 
 
 @app.route("/")
@@ -35,6 +48,22 @@ def search_todo():
             res_todos.append(todo)
 
     return render_template("todo.html", todos=res_todos)
+
+
+@app.route("/table")
+def table_view():
+    return render_template("table.html")
+
+
+@app.route("/button_indicator_test")
+def clicked_button():
+    time.sleep(2)
+    return "<p class='text-center'>congratulations, you clicked buttan!</p>"
+
+
+@app.route("/indicator")
+def indicator_view():
+    return render_template("indicator.html")
 
 
 if __name__ == "__main__":
